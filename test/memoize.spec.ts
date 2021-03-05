9ce30d89ca53891a8a3aebca22242450b5c3ecdf
Alex Kit <alex.kit@atmajs.com>
2021-03-05 10:51:36 +0100

    publish: add (memoize test) for clearOnReady

--- test/memoize.spec.ts
+++ test/memoize.spec.ts
@@ -1,15 +1,16 @@
 import { class_EventEmitter } from 'atma-utils'
 import { deco_memoize } from '../src/deco/memoize';
 import sinon = require('sinon');
+import { fn_memoize } from '../src/fn/memoize';
 
 UTest({
-    'memoize' () {
+    'memoize'() {
         const innerFn = sinon.spy(function (letter) {
             return letter.toUpperCase();
         });
         class Foo {
             @deco_memoize()
-            foo (letter) {
+            foo(letter) {
                 return innerFn(letter)
             }
         }
@@ -28,13 +29,13 @@ UTest({
         eq_(f.foo('a'), 'A');
         eq_(innerFn.callCount, 3);
     },
-    'memoize with maxAge' (done) {
+    'memoize with maxAge'(done) {
         const innerFn = sinon.spy(function (letter) {
             return letter.toUpperCase();
         });
         class Foo {
             @deco_memoize({ maxAge: .100 })
-            foo (letter) {
+            foo(letter) {
                 return innerFn(letter)
             }
         }
@@ -53,14 +54,14 @@ UTest({
             done();
         }, 200);
     },
-    'memoize with invalidation' () {
+    'memoize with invalidation'() {
         const innerFn = sinon.spy(function (letter) {
             return letter.toUpperCase();
         });
         const invalidation = new class_EventEmitter()
         class Foo {
-            @deco_memoize({ monitors: [ invalidation ] })
-            foo (letter) {
+            @deco_memoize({ monitors: [invalidation] })
+            foo(letter) {
                 return innerFn(letter)
             }
         }
@@ -78,7 +79,7 @@ UTest({
         eq_(innerFn.callCount, 2);
     },
     'memoize with clear on': {
-        async 'using async' () {
+        async 'using async'() {
             const arr = [2, 1, 2, 4, 2];
             const innerFn = sinon.spy(function (letter) {
                 return arr.shift();
@@ -86,7 +87,7 @@ UTest({
             const clearOn = sinon.spy(x => x === 2);
             class Foo {
                 @deco_memoize({ clearOn })
-                async foo () {
+                async foo() {
                     return innerFn()
                 }
             }
@@ -103,7 +104,7 @@ UTest({
             eq_(innerFn.callCount, 2);
             deepEq_(clearOn.args, [[2], [1]]);
         },
-        async 'using sync flow' () {
+        async 'using sync flow'() {
             const arr = [2, 1, 2, 4, 2];
             const innerFn = sinon.spy(function (letter) {
                 return arr.shift();
@@ -111,7 +112,7 @@ UTest({
             const clearOn = sinon.spy(x => x === 2);
             class Foo {
                 @deco_memoize({ clearOn })
-                foo () {
+                foo() {
                     return innerFn()
                 }
             }
@@ -127,5 +128,37 @@ UTest({
             eq_(clearOn.callCount, 2);
             eq_(innerFn.callCount, 2);
         }
+    },
+    async 'memoize only when active promise'() {
+        let num = 1;
+
+        const innerFn = sinon.spy(fn_memoize(async function (letter) {
+            let x = ++num;
+            await wait(500);
+            return x;
+        }, { clearOnReady: true }));
+
+        let promise1 = innerFn();
+
+        await wait(10);
+        let promise2 = innerFn();
+
+        await wait(600);
+        let promise3 = innerFn();
+
+        let result1 = await promise1;
+        eq_(result1, 2);
+
+        let result2 = await promise2;
+        eq_(result2, 2);
+
+        let result3 = await promise3;
+        eq_(result3, 3);
     }
 })
+
+async function wait(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    })
+}
