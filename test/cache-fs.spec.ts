f28276fc0d37639966fc44273140ce8cf5955d94
Alex Kit <alex.kit@atmajs.com>
2021-06-25 13:07:52 +0200

    test (multi writes)

--- test/cache-fs.spec.ts
+++ test/cache-fs.spec.ts
@@ -8,6 +8,9 @@ UTest({
     $before () {
         File.exists(path) && File.remove(path);
     },
+    $teardown () {
+        File.exists(path) && File.remove(path);
+    },
     async 'fs cache' () {
         let cache = new Cache({
             persistance: new FsTransport({ path: path })
@@ -18,8 +21,28 @@ UTest({
         await cache.flushAsync();
 
 
-        let data = await File.readAsync <string> (path, { skipHooks: true });
+        let data = await File.readAsync <string> (path, { skipHooks: true, cached: false });
         has_(data, String(time));
+    },
+    async 'fs cache multi writes' () {
+        let cache = new Cache({
+            persistance: new FsTransport({ path: path })
+        });
+
+        let rand1 = (Math.random() * 10 ** 5) | 0;
+        await cache.setAsync(`timespan.${rand1}`, rand1);
+
+        let rand2 = (Math.random() * 10 ** 5) | 0;
+        await cache.setAsync(`timespan.${rand2}`, rand2);
+
+        let rand3 = (Math.random() * 10 ** 5) | 0;
+        await cache.setAsync(`timespan.${rand3}`, rand3);
+
+        await cache.flushAsync();
+        let data = await File.readAsync <string> (path, { skipHooks: true, cached: false });
+        has_(data, String(rand1));
+        has_(data, String(rand2));
+        has_(data, String(rand3));
         console.log(data);
     }
 })
